b552000e26f701080c22a8bf4c9633475b2ecf41
ThienCao <thiencao@ThienCaos-MacBook-Pro.local>
2026-03-09 20:20:30 +0700

    clean some strategy

--- src/strategies/index.js
+++ src/strategies/index.js
@@ -2,30 +2,16 @@
 // Để thêm strategy mới: import + push vào STRATEGIES array.
 // Không cần sửa engine hay UI.
 
-import { PivotReversalStrategy }    from "./pivotReversal";
-import { PivotReversalAtrStrategy } from "./pivotReversalAtr";
-import { PivotReversalRsiStrategy } from "./pivotReversalRsi";
-import { MomentumZigzagStrategy }   from "./momentumZigzag";
-import { PrpPivotPsarStrategy }     from "./prpPivotPsar";
-import { ChannelBreakoutStrategy }  from "./channelBreakout";
-import { MomentumStrategy }         from "./momentum";
-import { CenteredRsiCrossStrategy } from "./centeredRsiCross";
-import { Ema200TrendlineBreakoutStrategy } from "./ema200TrendlineBreakout";
-import { PivotSupertrendStrategy }  from "./pivotSupertrend";
-import { PivotHiloFilteringStrategy } from "./pivotHiloFiltering";
+import { PivotReversalStrategy }   from "./pivotReversal";
+import { PrpPivotPsarStrategy }    from "./prpPivotPsar";
+import { ChannelBreakoutStrategy } from "./channelBreakout";
+import { MomentumStrategy }        from "./momentum";
 
 export const STRATEGIES = [
   PivotReversalStrategy,
-  PivotReversalAtrStrategy,
-  PivotReversalRsiStrategy,
-  MomentumZigzagStrategy,
   PrpPivotPsarStrategy,
   ChannelBreakoutStrategy,
   MomentumStrategy,
-  CenteredRsiCrossStrategy,
-  Ema200TrendlineBreakoutStrategy,
-  PivotSupertrendStrategy,
-  PivotHiloFilteringStrategy,
 ];
 
 export const STRATEGY_MAP = Object.fromEntries(
